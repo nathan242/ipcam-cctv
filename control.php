3cdbe32f7c200b5a7f84d827f70c32d023ba0b90
Nathan Patrizi <nathan.patrizi@gmail.com>
2019-04-27 15:59:39 +0100

    Fix control script

--- control.php
+++ control.php
@@ -1,28 +1,27 @@
 <?php
-require_once $_SERVER['DOCUMENT_ROOT'].'/include/main.php';
-switch ($_GET['action']) {
-    case "startall":
-        exec('php -f '.$_SERVER['DOCUMENT_ROOT'].'/control/start-all.php');
-        break;
-    case "stopall":
-        exec('php -f '.$_SERVER['DOCUMENT_ROOT'].'/control/stop-all.php');
-        break;
-    case "reloadall":
-        exec('php -f '.$_SERVER['DOCUMENT_ROOT'].'/control/reload-all.php');
-        break;
-    case "start":
-        exec('php -f '.$_SERVER['DOCUMENT_ROOT'].'/control/start-single.php '.$_GET['camera']);
-        break;
-    case "stop":
-        exec('php -f '.$_SERVER['DOCUMENT_ROOT'].'/control/stop-single.php '.$_GET['camera']);
-        break;
-    case "reload":
-        exec('php -f '.$_SERVER['DOCUMENT_ROOT'].'/control/reload-single.php '.$_GET['camera']);
-        break;
-}
-if (isset($_SERVER['HTTP_REFERER'])) {
-    //echo '<html><meta HTTP-EQUIV="REFRESH" content="0; url='.$_SERVER['HTTP_REFERER'].'"></html>';
-    header('Location: '.$_SERVER['HTTP_REFERER']);
-	exit;
-}
-?>
+    require_once 'include/inc.main.php';
+    switch ($_GET['action']) {
+        case "startall":
+            exec('php -f '.$_SERVER['DOCUMENT_ROOT'].'/control/start-all.php');
+            break;
+        case "stopall":
+            exec('php -f '.$_SERVER['DOCUMENT_ROOT'].'/control/stop-all.php');
+            break;
+        case "reloadall":
+            exec('php -f '.$_SERVER['DOCUMENT_ROOT'].'/control/reload-all.php');
+            break;
+        case "start":
+            exec('php -f '.$_SERVER['DOCUMENT_ROOT'].'/control/start-single.php '.$_GET['camera']);
+            break;
+        case "stop":
+            exec('php -f '.$_SERVER['DOCUMENT_ROOT'].'/control/stop-single.php '.$_GET['camera']);
+            break;
+        case "reload":
+            exec('php -f '.$_SERVER['DOCUMENT_ROOT'].'/control/reload-single.php '.$_GET['camera']);
+            break;
+    }
+    
+    if (isset($_SERVER['HTTP_REFERER'])) {
+        header('Location: '.$_SERVER['HTTP_REFERER']);
+        exit;
+    }
