64fe02508194444db1232fab55cc50ae6da3548c
Nathan Patrizi <nathan.patrizi@gmail.com>
2019-05-06 12:21:58 +0100

    Move editor table handling logic on camera editor

--- camera-manager.php
+++ camera-manager.php
@@ -1,6 +1,28 @@
 <?php
     require_once 'include/inc.main.php';
     
+    $editor = new editortable('CAMERAS', 'id');
+    $editor->handle_edit(
+        function (&$db, $id, $field, $value) {
+            $config = new config($db, $id);
+            $camera = new camera($db, $config->config_data, $id);
+            if (false === $camera->device_data || $camera->is_active()) {
+                return false;
+            }
+
+            $camera->device_data[$field] = $value;
+            return $camera->update();
+        },
+        array(&$db)
+    );
+    $editor->handle_delete(
+        function (&$db, $id) {
+            $config = new config($db, $id);
+            return camera::delete_camera($db, $config->config_data, $_POST['id']);
+        },
+        [&$db]
+    );
+
     $add_form = new form('ADD CAMERA', 'Add');
     $add_form->input('add', 'add', 'hidden', false, '1');
     foreach (camera::get_valid_properties() as $field_name) {
@@ -30,29 +52,6 @@
     }
     
     $devices = escaper::escape_html_array(camera::get_all($db));
-    
-    $editor = new editortable('CAMERAS', 'id');
-    $editor->handle_edit(
-        function (&$db, $id, $field, $value) {
-            $config = new config($db, $id);
-            $camera = new camera($db, $config->config_data, $id);
-            if (false === $camera->device_data || $camera->is_active()) {
-                return false;
-            }
-            
-            $camera->device_data[$field] = $value;
-            return $camera->update();
-        },
-        array(&$db)
-    );
-    $editor->handle_delete(
-        function (&$db, $id) {
-            $config = new config($db, $id);
-            return camera::delete_camera($db, $config->config_data, $_POST['id']);
-        },
-        [&$db]
-    );
-    
     $editor->set_data($devices, true);
     
     $headings = [];
